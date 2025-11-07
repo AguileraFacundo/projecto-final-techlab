856155bb9beda2a045738b35f4a1048cf21455f2
facundo <facundoaguileradev@gmail.com>
2025-11-07 19:33:22 -0300

    cors setup

--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ import express from 'express'
 import authRoutes from './routes/authRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import { authMiddleware } from './middleware/authMiddleware.js'
+import { cors } from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/api/auth', authRoutes)
 app.use('/api/products', authMiddleware, productRoutes)
 
