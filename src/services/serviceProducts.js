2ae12455f6be899ee96e095ff74d6f0feb7ed102
facundo <facundoaguileradev@gmail.com>
2025-11-07 19:18:27 -0300

    repository fix

--- src/services/serviceProducts.js
+++ src/services/serviceProducts.js
@@ -1,43 +1,27 @@
 import db from "../firebase.js"
+import { create, 
+        findOne, 
+        findAll,
+        updateOne, 
+        deleteOne} from '../repositories/products.js'
 
-
-const collectionName = "products"
 
 export const createProduct = async (productData) => {
-    const docRef = await db.collection(collectionName).add({
-            ...productData,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        });
-
-    return { id: docRef.id, ...productData }
+    return await create(productData)
 }
 
 export const findById = async (productId) => {
-    const doc = await db.collection(collectionName).doc(productId).get()
-    if (!doc.exists) return null;
-    
-    return { id: doc.id, ...doc.data() }
+    return await findOne(productId)
 }
 
 export const allProducts = async (filters = {}) => {
-    const query = db.collection(collectionName)
-    if (filters.category) {
-        query = query.where("category", "==", filters.category)
-    }
-    const snap = await query.get();
-
-    return snap.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+    return await findAll(filters)
 }
 
 export const updateProduct = async (productId, updateData) => {
-        await db.collection(collectionName).doc(productId).update({
-            ...updateData,
-            updatedAt: new Date()
-        })
-        return findById(productId)
+   return await updateOne(productId, updateData)
 }
 
 export const deleteProduct = async (productId) => {
-    await db.collection(collectionName).doc(productId).delete()
+    return await deleteOne(productId)
 }
