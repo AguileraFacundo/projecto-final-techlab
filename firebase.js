3404d2fbf27aa9b6d96defbff64936682a41bf33
facundo <facundoaguileradev@gmail.com>
2025-11-24 11:52:22 -0300

    Add credential objects for testing in Vercel

--- firebase.js
+++ firebase.js
@@ -1,6 +1,19 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import creds from './creds.json' with { type: 'json' };
+
+const creds = {
+    "type": process.env.TYPE,
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY,
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER,
+    "client_x509_cert_url": process.env.CLIENT_CERT_URL,
+    "universe_domain": process.env.UNIVERSE_DOMAIN
+};
 
 initializeApp({
     credential: cert(creds)
